1664768caec37cabbec845d8005cbd7d316c4194
Rohit Raj <rajrohit7388@outlook.com>
2026-01-31 17:30:33 +0530

    Add files via upload

---
+++ inputPrint.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int arr[5];
+    for(int i=0; i<5; i++)
+    {
+        printf("enter element %d : ", i+1);
+        scanf("%d", &arr[i]);
+    }
+    for(int i=0; i<5; i++)
+    {
+        printf("%d ",arr[i]);
+    }
+}
