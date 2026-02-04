5b2b3e5ef913b1344bf20c5327f03655d8d1dbe8
Rohit Raj <rajrohit7388@outlook.com>
2026-02-04 16:43:35 +0530

    Add files via upload

---
+++ twoDArray.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+    int arr[2][2];
+
+    for(int i=0; i<2; i++)
+    {
+        for(int j=0; j<2; j++)
+        {
+            printf("enter element [%d][%d] : ", i, j);
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    for(int i=0; i<2; i++)
+    {
+        for(int j=0; j<2; j++)
+        {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+}
